cf0c0c537dd34cc6c1635f1b3e68cb08fab4b7c8
Illnas <dariosting@gmail.com>
2025-07-08 10:02:39 +0200

    Tutorial started

---
+++ app.js
@@ -0,0 +1,34 @@
+
+
+
+
+
+class toggleParagraph extends HTMLParagraphElement {
+    constructor() {
+        super();
+    }
+
+    connectedCallback() {
+        console.log("Custom element added to page.");
+    }
+
+    disconnectedCallback() {
+        console.log("Custom element removed from page.");
+    }
+
+    connectedMoveCallback() {
+        console.log("Custom element moved with moveBefore()");
+    }
+
+    adoptedCallback() {
+        console.log("Custom element moved to new page.");
+    }
+
+    attributeChangedCallback(name, oldValue, newValue) {
+        console.log(`Attribute ${name} has changed.`);
+    }
+
+}
+
+
+customElements.define("toggle-paragraph", toggleParagraph);
